5a070ca7f0855d997327d23f04f1a6ffe5d7ab80
daihuan0612 <daihuan0612@gmail.com>
2025-12-24 17:53:23 +0800

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -15,7 +15,7 @@
         if (!dom.mobileToolbarTitle) {
             return;
         }
-        dom.mobileToolbarTitle.textContent = "Solara";
+        dom.mobileToolbarTitle.textContent = "小苹果Music";
     }
 
     function updateMobileOverlayScrim() {
